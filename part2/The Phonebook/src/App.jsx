65f4fde2b2834f4f9b7f03ad6f8163fcf2bcdedb
RachitReserk <trachit752@gmail.com>
2024-01-13 14:30:46 +0530

    updated phonebook

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -4,6 +4,7 @@ import Filter from './Components/Filter'
 import Form from './Components/Form'
 import Persons from './Components/Persons'
 import axios from 'axios'
+import noteService from './services/note'
 
 function App() {
 
@@ -14,12 +15,11 @@ function App() {
   const [showFilter , setShow] = useState(true)
   
   useEffect(() => {
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
-    })
-  } , [])
+    noteService.getAll().then(response =>
+      {
+        setPersons(response.data)
+      })
+  } , [persons])
 
 
 
@@ -36,26 +36,46 @@ function App() {
     event.preventDefault()
     const NameObject = {
       name : newName,
-      phno : newNumber,
-      id: persons.length+1,
+      number : newNumber,
+      /* id: persons.length+1, */
     } 
-
+    let flagid = 0;
     let flag='true';
     for(let i=0;i<persons.length;i++)
     {
       if(NameObject.name == persons[i].name)
+      {
       flag='false'
+      flagid=persons[i].id
+      }
     }
     if(flag == 'true')
     {
-    setPersons(persons.concat(NameObject))
+    noteService
+    .create(NameObject)
+    .then( response =>
+      {
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+      })
+    /* setPersons(persons.concat(NameObject))
     setNewName('')
-    setNewNumber('')
+    setNewNumber('') */
     }
     else
     {
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
+      if(confirm(`${NameObject.name} is already added , want to replace the number ?`))
+      {
+       noteService
+       .update(flagid,NameObject)
+       .then(reponse =>
+        {
+          alert("UPDATED")
+        })
+      }
+      /* alert(`${newName} is already added to phonebook`)
+      setNewName('') */
     }
   }
   
@@ -72,8 +92,21 @@ function App() {
   const handleFilterChange = (event) =>
   {
   setNewFilter(event.target.value)
-  
-  }  
+  }
+   
+  const handleDelete = (event) =>
+  {
+   console.log(event.target.id)
+   if(confirm(`You sure want to delete ${event.target.value} ?`))
+   {
+    noteService
+    .deleteX(event.target.id)
+    .then(response =>
+      {
+        alert('deleted') 
+      })
+   }
+  }
  
   return (
     <>
@@ -81,7 +114,7 @@ function App() {
     <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} setShow={setShow}></Filter>
     <Form addName={addName} newName={newName} handleChange={handleChange} newNumber={newNumber} handleNumberChange={handleNumberChange}></Form>
     <h2>Numbers</h2>
-    <Persons NameToShow={NameToShow}></Persons>
+    <Persons NameToShow={NameToShow} handleDelete={handleDelete}></Persons>
     </>
   )
 }
--- part2/The Phonebook/src/Components/Persons.jsx
+++ part2/The Phonebook/src/Components/Persons.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Persons(props) {
   return (
     <div>
-    {props.NameToShow.map(names => <li key={names.id}>{names.name} {names.number}</li>)}
+    {props.NameToShow.map(names => <li key={names.id}>{names.name} {names.number} <button onClick={props.handleDelete} value={names.name} id={names.id}>delete</button></li>)}
     </div>
   )
 }
